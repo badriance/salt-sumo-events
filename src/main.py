d7f39993991a3996bd5f789a13128136805176dc
Brian Adriance <badriance@vistaprint.com>
2017-12-05 13:55:03 -0500

    Functional, but not all that well factored python code

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@
 import sys
 import salt_event_forwarder
 
-if (len(sys.argv) != 3):
-  print "Usage: main.py <Salt-api-event-url> <sumologic-hosted-collector-url>"
+if (len(sys.argv) < 3 or len(sys.argv) > 4):
+  print "Usage: main.py <Salt-api-event-url> <sumologic-hosted-collector-url> [--ignore-ssl]"
 else:
-  f = salt_event_forwarder.SaltEventForwarder(sys.argv[1], sys.argv[2])
+  f = salt_event_forwarder.SaltEventForwarder(sys.argv[1], sys.argv[2], len(sys.argv) == 4 and sys.argv[3] == "--ignore-ssl")
   f.run()
--- src/salt_event_forwarder.py
+++ src/salt_event_forwarder.py
@@ -1,8 +1,41 @@
+import json
+import requests
+
 class SaltEventForwarder:
   
-  def __init__(self, salt_api_event_url, sumo_hosted_collector):
+  def __init__(self, salt_api_event_url, sumo_hosted_collector, ssl_no_verify):
     self.salt_api_event_url = salt_api_event_url
     self.sumo_hosted_collector = sumo_hosted_collector
-  
+    self.ssl_no_verify = ssl_no_verify
+    self.skip_logging_tags = ["salt/auth"]
+
   def run(self):
-    print ("run")
+    print("Starting request to stream events from salt...")
+    s = requests.Session()
+    if (self.ssl_no_verify):
+      s.verify = False
+    tag = ""
+    data = ""
+    try:
+      for line in self.streaming(s):
+        if line.startswith("tag:"):
+          if line.replace("tag:", "").strip() in self.skip_logging_tags:
+            continue
+          tag = line[4:].strip()
+        elif tag and line.startswith("data:"):
+          data = line[5:].strip()
+          requests.post(self.sumo_hosted_collector, data = {'tag': tag, 'data': data})
+          print("Posted {0} to sumo with {1}\n\n".format(tag, data))
+          tag = ""
+    except KeyboardInterrupt:
+      print "Interrupt signal received. Exiting."
+
+  def streaming(self, session):
+    headers = {'connection': 'keep-alive', 'content-type': 'application/json', 'transfer-encoding': 'chunked'}
+    request = requests.Request('GET', self.salt_api_event_url, headers = headers).prepare()
+    response = session.send(request, stream = True)
+    json_string = ''
+    for line in response.iter_lines():
+      if line:
+        yield line.strip()
+
